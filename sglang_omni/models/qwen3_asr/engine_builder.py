ddda2556a595ac134e44b794fc7e71cb54fe513b
Jiaxuan Luo(Gavin) <963500310@qq.com>
2026-08-09 14:06:45 -0700

    experiment(qwen3-asr): use flashinfer sampling backend

--- sglang_omni/models/qwen3_asr/engine_builder.py
+++ sglang_omni/models/qwen3_asr/engine_builder.py
@@ -108,7 +108,7 @@ class Qwen3ASREngineBuilder(AsrEngineBuilder):
             "mem_fraction_static": self.mem_fraction_static,
             "max_prefill_tokens": 4096,
             "chunked_prefill_size": 4096,
-            "sampling_backend": "pytorch",
+            "sampling_backend": "flashinfer",
             "dtype": dtype,
         }
         if self.mm_attention_backend is not None:
